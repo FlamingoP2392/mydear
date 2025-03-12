6848bb42bf7ca491469a2fd977b58b5e11f2ca09
FlamingoP2392 <pinping2392@gmail.com>
2025-03-13 00:12:33 +0700

    first commit

---
+++ login-to-mydear/script.js
@@ -0,0 +1,42 @@
+const statusImage = document.getElementById("statusImage");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginForm = document.getElementById("LoginForm");
+const submitBtn = document.getElementById("submitBin");
+
+const successLove = document.getElementById("successLove");
+const closeLove = document.getElementById("closeLove");
+
+// ป้องกันการส่งฟอร์มถ้าไม่มีข้อมูล
+loginForm.addEventListener("submit", function(event) {
+    if (usernameInput.value.trim() === "" || passwordInput.value.trim() === "") {
+        alert("กรอกข้อมูลให้ครบก่อน!");
+        statusImage.src = "./img/pan.jpg";
+        event.preventDefault();
+        return;
+    }
+
+    // ตรวจสอบรหัสผ่าน
+    if (passwordInput.value !== "07122022") {
+        alert("รหัสผ่านคือวันครบรอบจ้า! ปี ค.ศ. นะะะ");
+        statusImage.src = "./img/baseboll.jpg";
+        event.preventDefault();
+        return;
+    }
+
+    // ถ้าผ่านเงื่อนไขทั้งหมด แสดง successLove
+    successLove.style.display = "flex";
+    event.preventDefault(); // ป้องกันการรีเฟรชหน้า
+});
+
+// ปิด popup
+closeLove.addEventListener("click", function() {
+    successLove.style.display = "none";
+});
+
+// ปิด popup เมื่อคลิกนอกกรอบ
+window.addEventListener("click", function (event) {
+    if (event.target === successLove) {
+        successLove.style.display = "none";
+    }
+});
